5264adaa383a6db844646da8c0587fdf34697fea
CookieKastanie <jerem0205@yahoo.fr>
2022-05-08 10:47:43 +0200

    ...

---
+++ Akila/libs/stbimage/src/stb_image.cpp
@@ -0,0 +1,9 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_TRUETYPE_IMPLEMENTATION
+
+#include "stbimage/stb_image.h"
+#include "stbimage/stb_image_resize.h"
+#include "stbimage/stb_image_write.h"
+#include "stbimage/stb_truetype.h"
